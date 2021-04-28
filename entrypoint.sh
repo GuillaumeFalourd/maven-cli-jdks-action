5dab893666df99f5b79fbfee833628759eb5c3cf
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-28 13:36:52 -0300

    update path environment variable
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [[ -z $DIRECTORY ]]; 
+if [[ -z $DIRECTORY_PATH ]]; 
 then
-    echo "DIRECTORY env isn't set. Running in current directory"
+    echo "DIRECTORY_PATH env isn't set. Running in current directory"
 else
     echo "Moving to specified directory path"
-    cd $DIRECTORY
+    cd $DIRECTORY_PATH
 fi
 
 echo "Executing command: mvn $1"
